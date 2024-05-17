1ce56a332d33106807077bae2a5f356819ea8cd0
Mateus Alves <alvesmateus.ps@gmail.com>
2024-05-16 22:57:54 -0300

    Exercício simples para manipular vetores

---
+++ curso-java-nelio-alves/NegativosExercicioVetores/src/Application/Negativos.java
@@ -0,0 +1,34 @@
+package Application;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Negativos {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Quantos numeros voce vai digitar? ");
+		int n = sc.nextInt();
+		
+		int[] vetor = new int[n];
+		
+		for(int i = 0; i < vetor.length; i++) {
+			System.out.print("Digite um valor: ");
+			vetor[i] = sc.nextInt();
+		}
+		
+		System.out.println("Numeros negativos: ");
+		
+		for(int i = 0; i < vetor.length; i++) {
+			if(vetor[i] < 0) {				
+				System.out.printf("%d\n", vetor[i]);
+			}
+		}
+		
+		
+		sc.close();
+	}
+
+}
